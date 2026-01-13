2701de58ebb18cff52171e150071d1d4c3c587e6
BestBaconAround <officialcammy14@gmail.com>
2026-01-12 19:57:51 -0700

    Finished exersice 1

---
+++ week01/Exercise1/notes.cs
@@ -0,0 +1,11 @@
+//        string color = Console.ReadLine();
+//        Console.Write("What is your favorite color? ");
+//        Console.WriteLine(color);
+//        int x = 10;
+//        int y = 20;
+//        if (x > y)
+//        {
+//           Console.WriteLine("Greater");
+//        }
+//        string school = "BYU";
+//        Console.WriteLine($"I am studying at {school}");
