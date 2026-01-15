2b608c842b63ab8dfecd3a72537c588282990c01
BestBaconAround <officialcammy14@gmail.com>
2026-01-14 19:37:33 -0700

    Finished Exerscise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,10 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
+        //get user's grade
+        Console.WriteLine("What is your grade? ");
         string userGrade = Console.ReadLine();
-        Console.Write("What is your grade? ");
-        
-
-
+        //user grade
+        int grade = int.Parse(userGrade);
+        //creates the letter grade
+        string letter;
+        //checks grade and outputs letter
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        //checks if the user passes or fails the class
+        if (grade >= 70)
+        {
+            Console.WriteLine($"You Passed the course with a {letter}!");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up! Study harder and try again!");
+        }
     }
 }
