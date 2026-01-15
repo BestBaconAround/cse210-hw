b9335d1f3252f81cd3f1084d2c954db028c3b844
BestBaconAround <officialcammy14@gmail.com>
2026-01-15 16:47:04 -0700

    Finished exercise 3, but had the computer generate random number. Not user

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //user guess starts at -1 for loop
+        int numGuess = -1;
+        //computer creates a random number
+        Random rng = new();
+        int randNum = rng.Next(0, 101);
+
+        while (numGuess != randNum)
+        {
+            //gets the users number guess
+            Console.Write("What is the magic number? ");
+            string userInput = Console.ReadLine();
+            //checks if input is valid int
+            if (!int.TryParse(userInput, out numGuess))
+            {
+                Console.WriteLine("Enter a valid number.");
+                continue;
+            }
+            //checks user input if its higher or lower
+            if (numGuess > randNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (numGuess < randNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine($"Correct! the computer guess is {randNum}. Your guess was {userInput}");
+            }
+        }
     }
 }
