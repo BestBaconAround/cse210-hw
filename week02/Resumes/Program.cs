612d574fadcbabcadf725d897f690170b4d29313
BestBaconAround <officialcammy14@gmail.com>
2026-01-18 22:12:38 -0700

    step 7 done

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,10 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
+        //creates new instance
         Job job1 = new Job();
+        //job1 method
         job1._jobTitle = "Software Engineer";
         job1._company = "Lion Energy";
         job1._startYear = "2025";
         job1._endYear = "2026";
+        //job2 method
+        Job job2 = new Job();
+        job2._jobTitle = "Quality tester";
+        job2._company = "Apple";
+        job2._startYear = "2020";
+        job1._endYear = "2025";
+        //displays Job method
+        job1.Display();
+        //create new instance
+        Resume myResume = new Resume();
+        myResume._name = "Mary";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
     }
 }
