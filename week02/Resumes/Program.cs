264a2c950246c9dcafdb88ab75a52dbabb5a2b36
BestBaconAround <officialcammy14@gmail.com>
2026-01-18 21:46:07 -0700

    On step 6

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Lion Energy";
+        job1._startYear = "2025";
+        job1._endYear = "2026";
     }
 }
